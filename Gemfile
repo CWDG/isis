d72d5f79ec42ca621e439aab3664a782cfcc1085
Alex Burkhart <saterus@gmail.com>
2012-11-05 23:34:47 -0500

    New agency.

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+gem 'jquery-rails'
+gem 'haml-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :production do
+  gem 'pg'
+end
+
+group :development do
+  gem 'sqlite3'
+  gem 'pry'
+  gem 'pry-remote'
+  gem 'pry-nav'
+  gem 'awesome_print'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Isis::Application.config.secret_token = 'b3cb7ee426c7a42628daf768ec93814c553b3b94609bc96e01419abcf60394d9dce96e246aac5808084b04ac9cbda0fb444e7f43a6a6e487fd766bcd8e085875'
